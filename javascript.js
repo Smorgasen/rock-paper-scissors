b1a63a6cbf8ca3a52c2feb7ebfe0e757c56233cf
Smorgasen <yaroslaw.ivanko@gmail.com>
2022-07-13 17:48:03 +0300

    Start day/night theme changer

--- javascript.js
+++ javascript.js
@@ -12,6 +12,29 @@ function disableBtn() {
     });
 }
 
+function setTheme(themeName) {
+    localStorage.setItem('theme', themeName);
+    document.documentElement.className = themeName;
+}
+
+function toggleTheme() {
+    if (localStorage.getItem('theme') === 'theme-dark') {
+        setTheme('theme-light');
+    }
+    else {
+        setTheme('theme-dark');
+        document.getElementById('switch').toggle("fa-thumbs-down");
+    }
+}
+
+(function () {
+        if (localStorage.getItem('theme') === 'theme-dark') {
+            setTheme('theme-dark');
+        } else {
+            setTheme('theme-light');
+        }
+    })();
+
 function computerPlay() {
     options = ['Rock', 'Paper', 'Scissors'];
     computerChoice = options [Math.floor(Math.random() * options.length )];
